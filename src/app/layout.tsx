3903c5419161a0a05c8221d75d952c812960a1be
nikola <nikodola@gmail.com>
2025-03-28 00:49:35 +0100

    description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Horny-Rhino",
+  title: "Horny-Rhino | Official Store",
   description: "Official Store",
 };
 
